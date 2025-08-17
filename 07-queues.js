1db0b1d8622260c6acbe2551c35de8ee220fe520
Darryl Mendonez <darrylmendonez@new-host-2.home>
2025-08-17 00:43:07 -0400

    added queues

---
+++ 07-queues.js
@@ -0,0 +1,14 @@
+// Queues
+
+// FIFO - first in, first out
+
+// Enqueue - push elements to the end of a queue
+// Dequeue - removing elements from the begining or head of the list
+
+// Operation    Big-O Time
+// Enqueue        O(1)
+// Dequeue        O(1)
+
+// dequeue - just take the cur node and point it to the second item on the list. This makes it so that the first item is no longer the head of the list, the second item now is. Time complexity O(1).
+
+// Queues are much more simple when implememnted with Linked Lists as arrays can be very complicated when used as queueus.
